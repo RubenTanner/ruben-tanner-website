97ea02f5710aaf402730aef7868b561d8c04e3d2
RubenTanner <up2109969@myport.ac.uk>
2025-01-08 16:50:31 +0000

    new server stuff

--- server.js
+++ server.js
@@ -1,1 +1,29 @@
-//TODO blog API
+const express = require("express");
+const path = require("path");
+const subdomain = require("express-subdomain");
+const app = express();
+
+// Middleware for static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Subdomains
+const blogRouter = express.Router();
+const adminRouter = express.Router();
+
+blogRouter.get("/", (req, res) => res.send("Blog landing page"));
+adminRouter.get("/", (req, res) => res.send("Admin panel"));
+
+// Subdomain routing
+app.use(subdomain("blog", blogRouter));
+app.use(subdomain("admin", adminRouter));
+
+// Main site
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "public", "index.html"))
+);
+
+// Start the server
+const PORT = process.env.PORT || 80; // Use port 80 for HTTP
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
