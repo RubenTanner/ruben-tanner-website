37b8a9700cf9e929af88db8adbf796f02ac098ac
RubenTanner <up2109969@myport.ac.uk>
2025-01-18 11:31:09 +0000

    some updates and new projects

--- public/assets/script.js
+++ public/assets/script.js
@@ -1,6 +1,6 @@
 //welcome text
 document.addEventListener("DOMContentLoaded", function () {
-  const typewriterElement = document.getElementById("typewriter-heading");
+  const typewriterElement = document.querySelector("#typewriter-heading");
   const text = "Hey! Welcome to the party";
   typewriterElement.innerHTML = `<span class="typewriter">${text}</span>`;
 
@@ -21,4 +21,15 @@ document.addEventListener("DOMContentLoaded", function () {
   adjustFontSize();
   // Adjust on window resize
   window.addEventListener("resize", adjustFontSize);
+});
+
+// clear form when submitted
+const contactForm = document.querySelector("#contact form");
+const responseMessage = document.querySelector("#responseMessage");
+
+contactForm.addEventListener("submit", (event) => {
+  setTimeout(() => {
+    contactForm.reset();
+    responseMessage.textContent = "Oh cool! I'll get back to you soon :)";
+  }, 500); // give it a little bit for slow submissions
 });
